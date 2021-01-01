5540b93d3a33ef47ad23abd0235998b949cf2c9d
Ntchinda-Giscard <ntchinda1998@gmail.com>
2021-01-01 03:07:13 +0100

    text formatinf

--- app/dashboard/components/newEvents.tsx
+++ app/dashboard/components/newEvents.tsx
@@ -12,7 +12,6 @@ function NewEvents() {
                 <EventButton />
                 <EventButton />
                 <EventButton />
-                <EventButton />
             </div>
     
         </>
