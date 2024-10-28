f47b77659ebd92abc3dc23bb69caece51afc655d
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-10-28 13:45:22 +0100

    add leave button

--- app/dashboard/leave/page.tsx
+++ app/dashboard/leave/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@mantine/core";
+import { IconPlus } from "@tabler/icons-react";
+
 
 function Page() {
     return ( <>
        <main className="flex flex-col min-w-full min-h-full">
             <div className={"flex flex-col  md:flex-row justify-between mb-8"}>
                     <p style={{fontWeight: 800, fontSize: "large", color: "#404040"}}> Leaves </p>
-                        <Button  bg={"#16DBCC"}>
+                        <Button leftSection={ <IconPlus size={14} /> }  color={"#16DBCC"}>
                             Add Leaves
                         </Button>
                 </div>
