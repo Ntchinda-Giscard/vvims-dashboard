4342145fbeaa9b12484c1a7501886993d3072e1f
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-06 11:36:54 +0100

    leave approval

--- app/dashboard/leave/mutation/mutations.tsx
+++ app/dashboard/leave/mutation/mutations.tsx
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const INSERT_LEAVE = gql`
-mutation InsertLeave($comment: String = "", $employee_id: uuid = "", $end_date: date = "", $start_date: date = "", $leave_type: leave_type_enum!) {
-  insert_leaves_one(object: {comment: $comment, employee_id: $employee_id, end_date: $end_date, status: PENDING, start_date: $start_date, leave_type: $leave_type}) {
+mutation InsertLeave($comment: String!, $employee_id: uuid!, $end_date: date! , $start_date: date!, $leave_type: leave_type_enum!, $file_url: String = "", $file_name: String = "") {
+  insert_leaves_one(object: {comment: $comment, employee_id: $employee_id, end_date: $end_date, status: PENDING, start_date: $start_date, leave_type: $leave_type, fileByFile: {data: {file_url: $file_url, file_name: $file_name}}}) {
     id
   }
 }`;
@@ -17,15 +17,21 @@ mutation DeleteLeave($id: uuid! ) {
 
 
 export const ACCEPT_LEAVE = gql`
-mutation AcceptLeave($id: uuid!) {
+mutation AcceptLeave($id: uuid!, $approver_id: uuid! ) {
   update_leaves_by_pk(pk_columns: {id: $id}, _set: {status: ACCEPTED}) {
+    id
+  }
+  insert_leave_approval_one(object: {approval_status: ACCEPTED, approver_id: $approver_id, leave_id: $id}) {
     id
   }
 }`;
 
 export const REJECT_LEAVE = gql`
-mutation RejectLeave($id: uuid!) {
-  update_leaves_by_pk(pk_columns: {id: $id}, _set: {status: REJECTED}) {
+mutation RejectLeave($id: uuid!, $approver_id: uuid! , $comment: String!) {
+  update_leaves_by_pk(pk_columns: {id: $id}, _set: {status: REJECTED, comment: $comment}) {
+    id
+  }
+  insert_leave_approval_one(object: {approval_status: REJECTED, approver_id: $approver_id, leave_id: $id}) {
     id
   }
 }`;
