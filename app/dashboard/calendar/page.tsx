d6e6ce79cc8b6a465b125eba0f8b148c034cf6d0
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-22 21:25:55 +0100

    update remove

--- app/dashboard/calendar/page.tsx
+++ app/dashboard/calendar/page.tsx
@@ -1,10 +1,5 @@
 "use client"
 import {useEffect, useState} from "react";
-import { Scheduler } from "@bitnoi.se/react-scheduler"
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
 import { useNextCalendarApp, ScheduleXCalendar, useCalendarApp } from '@schedule-x/react';
 import {
   createViewDay,
@@ -17,42 +12,39 @@ import '@schedule-x/theme-default/dist/index.css';
 import { GET_EVENTS_CALENDAR} from "@/app/dashboard/calendar/query/query";
 import { useQuery } from "@apollo/client";
 import {useSelector} from "react-redux";
-
+import {convertToEventArray} from "@/app/dashboard/calendar/utils/convertToArray";
 
 export default function MyCalendar(){
-    const [isLoading, setIsLoading] = useState(false);
+    const [events, setEvents] = useState();
     const userInfo = useSelector((state: any) => state.auth.userInfo)
     const {data: dataEvent, loading: loadEvent, error: errEvents} = useQuery(GET_EVENTS_CALENDAR, {
         variables:{
             employee_id: userInfo?.employee?.id
         }
     })
-    const events = [
-      { id: '1', title: 'Meeting', start: '2024-11-21T10:00:00', end: '2024-11-21T11:00:00' },
-      { id: '2', title: 'Lunch', start: '2024-11-21T12:00:00', end: '2024-11-21T13:00:00' },
-    ];
+
     const plugins = [createEventsServicePlugin()]
-    const calendar = useCalendarApp({
+    const calendar = useNextCalendarApp({
       views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
       events: [
-        {
-          id: 'da77ee8e-bb7b-4789-a19c-2c0341f835dd',
-          title: 'Event 1',
-          start: '2024-11-22 09:00',
-          end: '2024-11-22 14:00',
-        },
+          {id: '1', title: 'Board meeting', start: '2024-11-29 08:00', end: '2024-11-29 09:00'},
+          {id: '2', title: 'Reunion des Directeur', start: '2024-11-29 11:00', end: '2024-11-29 12:00'}
       ],
       selectedDate: new Date().toISOString().split('T')[0]
     }, plugins)
+        // [
+        // {id: '1', title: 'Reunion des Directeur', start: "2024-11-29 11:00", end: "2024-11-29 12:00"}
+        // ]
 
     useEffect(() => {
-      setIsLoading(true);
 
       // fetching data
-      calendar.eventsService.getAll()
-      setIsLoading(false);
-      console.log("Events calendar :", dataEvent)
-    }, [calendar, dataEvent]);
+        const events = convertToEventArray(dataEvent)
+        setEvents(events)
+      // calendar.eventsService.getAll()
+
+      console.log('Events converted :', events)
+    }, [dataEvent]);
 
     
     return(
--- app/dashboard/calendar/utils/convertToArray.ts
+++ app/dashboard/calendar/utils/convertToArray.ts
@@ -1,7 +1,7 @@
 export const convertToEventArray = (data: any) => {
-    return data.events.map((event: { start_date: any; start_time: any; end_time: any; title: any; }, index: any) => {
-        const start = `${event.start_date} ${event.start_time}`;
-        const end = `${event.start_date} ${event.end_time}`;
+    return data?.events.map((event: { start_date: any; start_time: any; end_time: any; title: any; }, index: any) => {
+        const start = `${event?.start_date} ${ event?.start_time === event?.end_time ? null : event?.start_time.slice(0,5)}`;
+        const end = `${event?.start_date} ${ event?.start_time === event?.end_time ? null : event?.end_time.slice(0,5)}`;
         return {
             id: (index + 1).toString(), // Generating sequential IDs
             title: event.title,
