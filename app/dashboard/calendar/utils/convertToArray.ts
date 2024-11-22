ecd1352b9f31778842258e210fc8a1731ff8a902
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-22 17:11:36 +0100

    infer param

--- app/dashboard/calendar/utils/convertToArray.ts
+++ app/dashboard/calendar/utils/convertToArray.ts
@@ -1,7 +1,7 @@
 export const convertToEventArray = (data: any) => {
-    return data.events.map((event, index) => {
-        const start = `${event.start_date}T${event.start_time}`;
-        const end = `${event.start_date}T${event.end_time}`;
+    return data.events.map((event: { start_date: any; start_time: any; end_time: any; title: any; }, index: any) => {
+        const start = `${event.start_date} ${event.start_time}`;
+        const end = `${event.start_date} ${event.end_time}`;
         return {
             id: (index + 1).toString(), // Generating sequential IDs
             title: event.title,
