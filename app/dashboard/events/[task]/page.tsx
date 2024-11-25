e58b8fa0ae476e93572b1e70532ba9f5139c48d3
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-25 19:51:12 +0100

    add user

--- app/dashboard/events/[task]/page.tsx
+++ app/dashboard/events/[task]/page.tsx
@@ -4,10 +4,10 @@ import {useEffect} from 'react';
 import {GET_AGG_TASK, GET_TASK} from "@/app/dashboard/events/queries/get_events";
 import { useQuery, useSubscription } from '@apollo/client';
 import TaskTable from "@/app/dashboard/events/components/taskTable";
-import {ActionIcon, Button, Paper, Tabs} from '@mantine/core';
-import {IconArrowLeft} from "@tabler/icons-react";
+import {ActionIcon, Button, Paper, Tabs, rem} from '@mantine/core';
+import {IconArrowLeft, IconUsersGroup} from "@tabler/icons-react";
 import {useRouter} from "next/navigation";
-import classes from '@/app/dashboard/events/[task]/components/Header.module.css'
+import { IconPhoto, IconMessageCircle } from '@tabler/icons-react';
 
 export default function Page({params,}: { params: Promise<{ task: string }> }) {
     const router = useRouter();
@@ -19,13 +19,13 @@ export default function Page({params,}: { params: Promise<{ task: string }> }) {
     });
 
     const tabs = [
-        'Task',
-        'Employee event',
+        { label: 'Task', icon: IconPhoto},
+        {label: 'Employee event', icon: IconUsersGroup }
     ];
 
     const items = tabs.map((tab) => (
-        <Tabs.Tab value={tab} key={tab}>
-            {tab}
+        <Tabs.Tab value={tab.label} key={tab.label} leftSection={<tab.icon style={{ width: rem(12), height: rem(12) }} /> }>
+            {tab.label}
         </Tabs.Tab>
     ));
 
@@ -37,16 +37,9 @@ export default function Page({params,}: { params: Promise<{ task: string }> }) {
             <main className="flex flex-col min-w-full min-h-full">
                 <Tabs
                     defaultValue="Home"
-                    variant="outline"
-                    // visibleFrom="sm"
-                    // classNames={{
-                    //     root: classes.tabs,
-                    //     list: classes.tabsList,
-                    //     tab: classes.tab,
-                    // }}
                 >
                     <Tabs.List>{items}</Tabs.List>
-                    <Tabs.Panel value={tabs[0]}>
+                    <Tabs.Panel value={tabs[0].label}>
                         <div className={"flex flex-row mb-8"}>
                             <ActionIcon onClick={() => router.back()} color="#404040" variant="subtle"
                                         aria-label="Settings">
@@ -60,8 +53,14 @@ export default function Page({params,}: { params: Promise<{ task: string }> }) {
                             />
                         </Paper>
                     </Tabs.Panel>
-                    <Tabs.Panel value={tabs[1]}>
-                        Participants
+                    <Tabs.Panel value={tabs[1].label}>
+                        <div className={"flex flex-row mb-8"}>
+                            <ActionIcon onClick={() => router.back()} color="#404040" variant="subtle"
+                                        aria-label="Settings">
+                                <IconArrowLeft style={{width: '70%', height: '70%'}} stroke={1.5}/>
+                            </ActionIcon>
+                            <p style={{fontWeight: 800, fontSize: "large", color: "#404040"}}> Participants </p>
+                        </div>
                     </Tabs.Panel>
                 </Tabs>
 
