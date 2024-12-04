b62e964df4c3a13ca946e5aa937f879e67b9a94b
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-04 11:03:16 +0100

    importance to thier eye

--- app/dashboard/notifications/page.tsx
+++ app/dashboard/notifications/page.tsx
@@ -1,15 +1,21 @@
 "use client"
 import cx from "clsx";
 import classes from "@/app/dashboard/components/css/dashboard.module.css";
-import {Paper} from "@mantine/core";
+import {Box, Button, Divider, Group, Paper, ThemeIcon} from "@mantine/core";
 import {Poppins} from "next/font/google";
 import {useSelector} from "react-redux";
 import {useMutation, useSubscription} from "@apollo/client";
 import {GET_EVENT_NOTIF} from "@/app/dashboard/events/queries/event_notif";
 import {ACCEPT_EVENT, DECLINE_EVENTS} from "@/app/dashboard/events/mutation/insert_events";
+import {IconCalendar, IconUsers} from "@tabler/icons-react";
 
 
 const font_heading = Poppins({ subsets: ["latin"], weight:["500"] });
+
+const icons = {
+    EVENTS: <IconCalendar style={{ width: '70%', height: '70%' }} />,
+    VISITS: <IconUsers style={{ width: '70%', height: '70%' }} />
+}
 
 export default function Page(){
 
@@ -41,6 +47,25 @@ export default function Page(){
         })
     }
 
+    function formatTimestamp(timestamp: string): string {
+        const date = new Date(timestamp);
+
+        // Get components of the date
+        const options: Intl.DateTimeFormatOptions = { month: 'short', day: 'numeric', year: 'numeric' };
+        const formattedDate: string = date.toLocaleDateString('en-US', options); // e.g., "Nov 21, 2024"
+
+        const hours: string = date.getHours().toString().padStart(2, '0');
+        const minutes: string = date.getMinutes().toString().padStart(2, '0');
+
+        return `${formattedDate} - ${hours}:${minutes}`;
+    }
+
+    function hasUnreadNotifications(employeeNotifications: any) {
+        if(!employeeNotifications) return false
+        return employeeNotifications?.filter((n: { type: string; }) => n?.type !== 'MESSAGES').some((notification: { is_read: boolean; }) => notification.is_read === false);
+    }
+
+
     return(
         <>
             <main className="flex min-h-full flex-col gap-3">
@@ -49,6 +74,38 @@ export default function Page(){
                     shadow={'md'}
                     radius={'md'}
                 >
+                    {
+                        dataNotif?.employee_notifications?.filter((n: { type: string; }) => n?.type !== 'MESSAGES')?.map((n : any) =>(
+                            <Box py={10} px={20} key={n?.id} >
+                                <div className="flex flex-row gap-5">
+                                    <ThemeIcon variant="light" radius="xl" size="lg">
+
+                                        {
+                                            // @ts-ignore
+                                            icons[`${n?.type}`]
+                                        }
+                                    </ThemeIcon>
+                                    <div className="flex flex-col gap-1 w-full">
+                                        <div className="flex flex-col">
+                                            <p className={classes.notif_title}> {`${n?.action}`} {`${n?.event?.employee?.firstname}`} <span className={classes.notif_desc}> added you to</span> {`${n?.title}`} </p>
+                                        </div>
+                                        <div className='flex flex-row'>
+                                            <p className={classes.notif_time}> {formatTimestamp(n?.created_at)} </p>
+                                        </div>
+                                        {
+                                            n?.event?.event_participants?.[0]?.status !== 'PENDING' ?
+                                                null :
+                                                <Group>
+                                                    <Button loading={acceptLoad} onClick={() => acceptEventsPart(n?.event?.event_participants?.[0])} variant="filled" color="blue" radius="md" size="xs" > Accept </Button>
+                                                    <Button loading={declineLoad} onClick={() => declineEventsPart(n?.event?.event_participants?.[0])} variant="outline" color="blue" radius="md" size="xs" > Decline </Button>
+                                                </Group>
+                                        }
+                                    </div>
+                                </div>
+                                <Divider my={'xs'} />
+                            </Box>
+                        ))
+                    }
                 </Paper>
             </main>
         </>
